e8afe77d29c6caefa4ea2b8547e3c731d38a81f3
João Nogueira <joaoprnogueira@gmail.com>
2018-08-24 16:19:18 +0100

    Fix issue caused by loopback sometimes parsing count method as find

--- nested-filter.js
+++ nested-filter.js
@@ -1,107 +1,132 @@
 'use strict';
+// Routing
 
+// exports && core logic
 module.exports = function(Model, options) {
   Model.beforeRemote('find', function event(ctx, options, next) {
     /* Allows nested filters to be applied */
-    if (ctx.args.filter && ctx.args.filter.excludeIfEmpty === true) {
-      ctx.args.filter.excludeIfEmpty = generateExcludeArray(ctx.args.filter.include);
-    }
-    if (!ctx.args.filter) {
-      next();
-    } else if (ctx.args.filter.excludeIfEmpty) {
-      // console.log('<i> NestedSearch mixin was used');
-      // constants
-      ctx.originalFilter = JSON.parse(JSON.stringify(ctx.args.filter));
-      delete ctx.args.filter.limit;
-      delete ctx.args.filter.skip;
-      next();
+    if (ctx.methodString.split('.')[1] == 'count') {
+      /*
+       * Sometimes loopback screws up requests and parses requests with
+       * methodString = "CuttingProgram.count"
+       * but
+       * method.name = "find"
+       * which forwards count requets to find hook
+       */
+      beforeRemoteCount(ctx, options, next);
     } else {
-      next();
+      beforeRemoteFind(ctx, options, next)
     }
   });
-
+  
   Model.beforeRemote('count', function event(ctx, options, next) {
-    /* Allows nested filters to be applied */
-    if (ctx.args.filter && ctx.args.filter.excludeIfEmpty === true) {
-      ctx.args.filter.excludeIfEmpty = generateExcludeArray(ctx.args.filter.include);
-    }
-    if (!ctx.args.where) {
-      next();
-    } else if (ctx.args.where.excludeIfEmpty) {
-      // console.log('<i> NestedSearch mixin was used');
-      // constants
-      ctx.methodString.replace('count', 'find');
-      ctx.method.name = 'find';
-      ctx.args.filter = ctx.args.where;
-      ctx.originalFilter = JSON.parse(JSON.stringify(ctx.args.filter));
-      ctx.wasCount = true;
-      delete ctx.args.filter.limit;
-      delete ctx.args.filter.skip;
-      next();
-    } else {
-      next();
-    }
+    /* Allows nested filters to be applied - forwards as find method*/
+    beforeRemoteCount(ctx, options, next);
   });
-
+  
   Model.afterRemote('find', function event(ctx, options, next) {
-    function includesAreValid(result, excludes) {
-      return excludes.reduce((veredict, exclude) => {
-        function isEmpty(result, exlude) {
-          let curResult = result.__data[exclude[0]];
-          if (exclude.length == 1) {
-            if (!curResult) {
-              return false;
-            } else if (Array.isArray(curResult)) {
-              return (curResult.length > 0) ? true : false;
-            } else {
-              return true;
-            }
-          } else if (curResult) {
-            exclude.shift();
-            if (Array.isArray(curResult)) {
-              curResult = curResult.filter(result => includesAreValid(result, exclude));
-              return (curResult.length > 0) ? true : false;
-            } else {
-              return isEmpty(curResult, exclude);
-            }
-          } else {
-            return false;
-          }
-        }
-
-        exclude = exclude.split(/\.(.+)/).filter(Boolean);
-        return isEmpty(result, exclude) && veredict;
-      }, true);
-    }
-
-    if (!ctx.args.filter) {
-      next();
-    } else if (ctx.args.filter.excludeIfEmpty) {
-      ctx.result.count ? ctx.result = ctx.result.count : null;
-      let results = ctx.result;
-      const originalFilter = ctx.originalFilter;
-      const excludes = originalFilter.excludeIfEmpty;
-
-      results = results.filter(result => includesAreValid(result, excludes));
-      ctx.result = results;
-
-      if (originalFilter.skip) {
-        results.remove(0, originalFilter.skip - 1);
-      }
-      if (originalFilter.limit && results.length > originalFilter.limit) {
-        results.remove(originalFilter.limit, results.length - 1);
-      }
-
-      if (ctx.wasCount) {
-        // means we arrived here from a COUNT request via NestedSearch
-        ctx.result = {count: results.length};
-      }
-      next();
-    } else {
-      next();
-    }
+    afterRemoteFind(ctx, options, next);
   });
 };
+
+// Logic
+function beforeRemoteFind(ctx, options, next) {
+  if (ctx.args.filter && ctx.args.filter.excludeIfEmpty === true) {
+    ctx.args.filter.excludeIfEmpty = generateExcludeArray(ctx.args.filter.include);
+  }
+  if (!ctx.args.filter) {
+    next();
+  } else if (ctx.args.filter.excludeIfEmpty) {
+    // constants
+    ctx.originalFilter = JSON.parse(JSON.stringify(ctx.args.filter));
+    delete ctx.args.filter.limit;
+    delete ctx.args.filter.skip;
+    next();
+  } else {
+    next();
+  }
+}
+
+function beforeRemoteCount(ctx, options, next) {
+  if (ctx.args.filter && ctx.args.filter.excludeIfEmpty === true) {
+    ctx.args.filter.excludeIfEmpty = generateExcludeArray(ctx.args.filter.include);
+  }
+  if (!ctx.args.where) {
+    next();
+  } else if (ctx.args.where.excludeIfEmpty) {
+    // console.log('<i> NestedSearch mixin was used');
+    // constants
+    ctx.methodString.replace('count', 'find');
+    ctx.method.name = 'find';
+    ctx.args.filter = ctx.args.where;
+    ctx.originalFilter = JSON.parse(JSON.stringify(ctx.args.filter));
+    ctx.wasCount = true;
+    delete ctx.args.filter.limit;
+    delete ctx.args.filter.skip;
+    next();
+  } else {
+    next();
+  }
+}
+
+function afterRemoteFind(ctx, options, next) {
+  function includesAreValid(result, excludes) {
+    return excludes.reduce((veredict, exclude) => {
+      function isEmpty(result, exlude) {
+        let curResult = result.__data[exclude[0]];
+        if (exclude.length == 1) {
+          if (!curResult) {
+            return false;
+          } else if (Array.isArray(curResult)) {
+            return (curResult.length > 0) ? true : false;
+          } else {
+            return true;
+          }
+        } else if (curResult) {
+          exclude.shift();
+          if (Array.isArray(curResult)) {
+            curResult = curResult.filter(result => includesAreValid(result, exclude));
+            return (curResult.length > 0) ? true : false;
+          } else {
+            return isEmpty(curResult, exclude);
+          }
+        } else {
+          return false;
+        }
+      }
+
+      exclude = exclude.split(/\.(.+)/).filter(Boolean);
+      return isEmpty(result, exclude) && veredict;
+    }, true);
+  }
+
+  if (!ctx.args.filter) {
+    next();
+  } else if (ctx.args.filter.excludeIfEmpty) {
+    ctx.result.count ? ctx.result = ctx.result.count : null;
+    let results = ctx.result;
+    const originalFilter = ctx.originalFilter;
+    const excludes = originalFilter.excludeIfEmpty;
+
+    results = results.filter(result => includesAreValid(result, excludes));
+    ctx.result = results;
+
+    if (originalFilter.skip) {
+      results.remove(0, originalFilter.skip - 1);
+    }
+    if (originalFilter.limit && results.length > originalFilter.limit) {
+      results.remove(originalFilter.limit, results.length - 1);
+    }
+
+    if (ctx.wasCount) {
+      // means we arrived here from a COUNT request via NestedSearch
+      ctx.result = {count: results.length};
+    }
+    next();
+  } else {
+    next();
+  }
+}
 
 function generateExcludeArray(includes) {
   function checkNested(include, soFar) {
